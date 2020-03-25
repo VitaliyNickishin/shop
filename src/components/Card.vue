c0de19419bf38f18646e01140dd76b6e331df1f9
“VitaliyNickishin” <nickishin34@gmail.com>
2020-03-25 17:57:26 +0200

    communication between componets Catalog and Card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,9 +1,18 @@
 <template>
  <div class="card">
-  <img src="" alt="img">
+  <img 
+   :src="require('../assets/images/'+ product_data.image)" 
+   alt="img"
+   class="card__img"
+   />
   <p class="card__name">{{product_data.name}}</p>
-  <p class="card__price">Price: {{product_data.price}}</p>
-  <button class="card__btn btn">Add to cart</button>
+  <p class="card__price">Price: {{product_data.price}} грн</p>
+  <button 
+   class="card__btn btn" 
+   @click="sendArticleToParent"
+  >
+   Add to cart
+  </button>
  </div>
 </template>
 
@@ -17,6 +26,12 @@
      return {}
     }
    }
+  },
+  methods: {
+   //передадим родетлю Catalog артикул нажатой карточки
+   sendArticleToParent() {
+    this.$emit('sendArticleToParent', this.product_data.article)
+   }
   }
  }
 </script>
@@ -24,10 +39,12 @@
 <style lang="scss">
 
  .card {
-  // padding: $padding;
-  padding: 16px;
-  margin-bottom: 16px;
+  padding: $padding*2;
+  margin-bottom: $margin*2;
   flex-basis: 25%;
   box-shadow: 0 0 8px 0 #e0e0e0;
+  &__img {
+   width: 100px;
+  }
  }
 </style>
--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -6,6 +6,7 @@
     v-for="product in products"
     :key="product.article"
     :product_data="product"
+    @sendArticleToParent="reciveArticle"
    />
    
   </div>
@@ -27,7 +28,7 @@ import Card from './Card'
      {
       image: "1.jpg",
       name: "T-shirt 1",
-      price: 2100.234234234,
+      price: 2100,
       article: "T1",
       available: true,
       category: "Мужские"
@@ -35,7 +36,7 @@ import Card from './Card'
      {
       image: "2.jpg",
       name: "T-shirt 2",
-      price: 3500.234234234,
+      price: 3500,
       article: "T2",
       available: true,
       category: "Женские"
@@ -43,7 +44,7 @@ import Card from './Card'
      {
       image: "3.jpg",
       name: "T-shirt 3",
-      price: 2600.234234234,
+      price: 2600,
       article: "T3",
       available: true,
       category: "Мужские"
@@ -51,12 +52,34 @@ import Card from './Card'
      {
       image: "4.jpg",
       name: "T-shirt 4",
-      price: 3500.234234234,
+      price: 3500,
       article: "T4",
       available: true,
       category: "Женские"
      },
+     {
+      image: "5.jpg",
+      name: "T-shirt 5",
+      price: 2750,
+      article: "T5",
+      available: true,
+      category: "Мужские"
+     },
+     {
+      image: "6.jpg",
+      name: "T-shirt 6",
+      price: 3200,
+      article: "T6",
+      available: true,
+      category: "Женские"
+     },
     ]
+   }
+  },
+  methods: {
+   reciveArticle(hello) {
+    console.log(hello);
+    
    }
   }
   
